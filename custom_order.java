7c56b0834aa960cf784caf43a0b382da7d8098c7
Brittney Stewart <brittneystewart@Brittneys-MacBook-Pro.local>
2020-12-05 19:44:08 -0800

    add cupcake shop

--- custom_order.java
+++ custom_order.java
@@ -12,10 +12,23 @@ public class custom_order {
     // TEST CODE
     
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
-	  
+	Scanner keyboard = new Scanner(System.in );
+	
+	String firstName = "Odin"; // User's first name
+	String itemOrder = "vanilla"; // Item ordered
+	String frostingType = "none"; // Frosting ordered
+	String fillingType = "raspberry"; // Filling ordered
+	String toppings = "almonds"; // Toppings ordered
+	String input = "good"; // User input
+
+	double cost = 15.00; // Cost of cake and cupcakes
+	final double TAX_RATE = .08; // Sales tax rate
+	double tax; // Amount of tax 
 
 	 
     // Introduce shop and prompt user to input first name
+	System.out.println("Welcome to Java's Cake & Cupcake Shop!");
+	System.out.println("We make custom cakes with our secret cake batter!");
       
 
 	  
@@ -23,7 +36,20 @@ public class custom_order {
     // TEST CODE
     
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
-   
+   	System.out.println();
+	System.out.println(firstName + " , your order is as follows: ");
+	System.out.println("_________________________________________");
+	System.out.println("Item Ordered: " + itemOrder);
+	System.out.println("Frosting: " + frostingType);
+	System.out.println("Filling: " + fillingType);
+	System.out.println("Toppings: " + toppings);
+	System.out.println("_________________________________________");
+
+    // Cost and sales tax of order
+	System.out.printf("The cost of your order is: $%.2f\n", cost);
+	tax = cost * TAX_RATE;
+	System.out.printf("The tax is: $%.2f\n", tax);
+	System.out.printf("The total due is: $%.2f\n",(tax + cost));
 	  
       
     // TEST CODE     
